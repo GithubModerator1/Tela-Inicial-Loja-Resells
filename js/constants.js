8c374e7ebfe24f880f0300954aad9d8c910992a5
GithubModerator1 <autokillstreak@tuta.io>
2023-08-02 23:34:30 -0300

    chat-gpt-mito

---
+++ js/constants.js
@@ -0,0 +1,7 @@
+const ON_CHANGE_DEBOUNCE_TIMER = 300,
+  PUB_SUB_EVENTS = {
+    cartUpdate: "cart-update",
+    quantityUpdate: "quantity-update",
+    variantChange: "variant-change",
+    cartError: "cart-error",
+  };
